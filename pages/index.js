d7e8fde0edc955ba18a09bbd469852aeaf415281
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2023-05-28 06:33:36 -0400

    test

--- pages/index.js
+++ pages/index.js
@@ -17,10 +17,7 @@ export default function Home() {
         </h1>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+  
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
